2defc24d877686d84ab4562fb65daf86563c7cf1
Vaishnavi jadhav <149877484+Vaishu123j@users.noreply.github.com>
2023-11-09 15:59:53 +0530

    Update LoginTab.java

--- ProjectOrangeHRM/src/test/java/main/LoginTab.java
+++ ProjectOrangeHRM/src/test/java/main/LoginTab.java
@@ -15,66 +15,66 @@ import Base.LoginTest;
 
 public class LoginTab extends BaseClassTest {
 	WebDriver driver;
-
 	public LoginTest login;
 
 	@BeforeMethod
 	public void setup() {
 		driver = intialization("Chrome");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+	}
+        @Test
+	private void verifyLoginWithInavalidCredentials() {
 		LoginTest login = new LoginTest(driver);
 
 		login.enterUsername("Admin");
-		login.enterPassword("admin123");
+		login.enterPassword("23457");
 		login.clickLoginButton();
+		Assert.assertEquals(login.getErrorMessage(), "Invalid credentials");
+		driver.close();
+	}
+
+
+
+	@Test
+	public void verifyTitle() {
+		
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "OrangeHRM";
+
+		Assert.assertEquals(actualTitle, expectedTitle);
+		driver.close();
+	
+	}
+
+	@Test
+	public void Verifylogo() {
+		
+	
+		boolean flag = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]")).isDisplayed();
+
+		Assert.assertTrue(flag);
+		driver.close();
+	
 
 	}
 
+	@Test
+	public void testLoginWithValidCredentials() {
+		LoginTest login = new LoginTest(driver);
+		login.enterUsername("Admin");
+		login.enterPassword("admin123");
+		login.clickLoginButton();
+		System.out.println("successful login");
+		driver.close();
+		
+
+	}
 	@AfterMethod
 	public void end() {
 		driver.quit();
 
 	}
 
-	@Test
-	private void verifyLoginWithInavalidCredentials(String usrname, String password) {
-
-		login.enterUsername("Admin");
-		login.enterPassword("admin123");
-		login.clickLoginButton();
-		Assert.assertEquals(login.getErrorMessage(), "Invalid credentials");
-	}
-
-//		WebElement errorMessage = driver
-//				.findElement(By.xpath("///*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/p"));
-//		String actualErrorMessage = errorMessage.getText();
-//		String expectedErrorMessage = "Invalid credentials";
-//		Assert.assertEquals(actualErrorMessage, expectedErrorMessage,
-//				"Error message doesn't match for invalid credentials.");
-//	}
-
-	@Test
-	public void verifyTitle() {
-		String actualTitle = driver.getTitle();
-		String expectedTitle = "OrangeHRM";
-
-		Assert.assertEquals(actualTitle, expectedTitle);
-	}
-
-	@Test
-	public void Verifylogo() {
-		boolean flag = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]")).isDisplayed();
-
-		Assert.assertTrue(flag);
-
-	}
-
-	@Test
-	public void testLoginWithValidCredentials() {
-		login.enterUsername("Admin");
-		login.enterPassword("admin123");
-		login.clickLoginButton();
-
-	}
-
 }
+
